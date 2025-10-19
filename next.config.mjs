2abd4e3e419203e15a556adac67e1d07540c92f7
Om Singh Chouhan <omchouhan227@gmail.com>
2025-10-19 16:24:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This tells Next.js to use Webpack for the production build
+  reactStrictMode: true, // optional but recommended
+  turbopack: false,
   webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.builtins = config.builtins || {};
-      config.builtins.turbo = false; // disables Turbopack client-side
-    }
     return config;
   },
 };
